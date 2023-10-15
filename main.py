fff1cd2bad0b673d4debb1ba10bbed242c20423f
Logging4J <80828850+Logging4J@users.noreply.github.com>
2023-10-14 20:00:38 -0400

    Spacing

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import subprocess
 from datetime import datetime
 import utils.proc
 import colorama
+
 colorama.init()
+
 os.system('cls' if os.name == 'nt' else 'clear')
+
 def log(txt):
   print(f"({colorama.Fore.RED}λ{colorama.Style.RESET_ALL}) {txt}")
+
 log("⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣴⣦⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
 log("⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⠿⠿⠿⠿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀")
 log("⠀⠀⠀⠀⣠⣾⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣶⡀⠀⠀⠀⠀")
@@ -24,12 +28,19 @@ log("⠀⠀⠀⠻⣿⣿⣧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿
 log("⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⣄⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⠟⠁⠀⠀⠀⠀")
 log("⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀")    
 log("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠉⠉⠀⠀⠀⠀⠀    ⠀")
+
 time.sleep(2)
+
 log("starting lambda...")
+
 time.sleep(2)
+
 log("starting webserver...")
+
 time.sleep(2)
+
 log("webserver started at http://localhost:2077")
-os.system("python3 -m http.server -d $(pwd)/utils/interface/ 2077")
+
+os.system("python -m http.server -d $(pwd)/utils/interface/ 2077")
 
 
