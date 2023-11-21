bf13e25e253c95ecb622bcb1a464472882443428
C4T0P1A <walterthebot972@gmail.com>
2023-11-21 14:11:38 -0700

    C4T is based

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
 current_dir=$(pwd)
+rm -dfr .build
+mkdir .build
+ls
+cp -r $current_dir .build
+cd .build/Lambda/
+rm -dfr .git
+rm -dfr .github
+rm -dfr .build
+rm -dfr pull.sh
+rm -dfr push.sh
+rm -dfr rebase.sh
+rm -dfr README.md
+echo clear >> lambda.sh
--- utils/server.py
+++
@@ -1,7 +0,0 @@
-import http.server,os,sys,asyncio,socket,socketserver
-DIR = "./utils/interface/" 
-
-async def start(port,args=""):
-  print()
-async def stop(port):
- print()
